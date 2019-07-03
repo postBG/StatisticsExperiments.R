e9a4afd067a496ce0d8b6e97b17f8ef596fe6290
postBG <profile2697@gmail.com>
2019-07-03 21:33:25 +0900

    chap 3

---
+++ Chap3.R
@@ -0,0 +1,23 @@
+cdc_data = read.table('~/RStudioProjects/dataset/ch02/cdc.txt')
+
+# Transform genhlth to categorical data
+cdc_data$genhlth = factor(cdc_data$genhlth)
+
+# Ex1: Summary categorical data with table
+table(cdc_data$genhlth)
+
+# Ex2: Summary and mean of weights
+summary(cdc_data$weight)
+
+# Ex3: Scatter plot between weight and wtdesire
+plot(cdc_data$weight, cdc_data$wtdesire, main='Scatter Plot', xlab='weight', ylab='desired weight')
+cor(cdc_data$weight, cdc_data$wtdesire) # 0.8000521
+
+# Ex4: Analysis of the wdiff
+cdc_data$wdiff = cdc_data$wtdesire - cdc_data$weight
+summary(cdc_data$wdiff)
+plot(cdc_data$weight, cdc_data$wdiff)
+
+# Ex5: Histogram of the Age
+hist(cdc_data$age, breaks=50)
+hist(cdc_data$age, breaks=100)
